b709a5d8fbc9f1441d32825909dcf4b9f9b3c920
Luke <LukeSupan@outlook.com>
2026-02-17 16:47:49 -0500

    start of generic

---
+++ games/generic.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+from core.aggregation import update_comp_stats
+from core.models import make_player, make_comp
+from core.parsing import parse_game_line_generic
+from core.printing import print_non_role_comps, print_player_stats
+from core.config import GAME_CONFIGS
+
+def run(games):
+
+    role_labels = GAME_CONFIGS["generic"]
+    player_stats = defaultdict(make_player)
+    comp_stats = defaultdict(make_comp)
+
+    # aggregate stats for hero shooters
+    for line in games:
+        team, result = parse_game_line_generic(line)
+
+        # update_player_stats_generic(player_stats, team, result) # each player
+        update_comp_stats(comp_stats, team, result) # each comp, regardless of role
+
+    # printing final results
+    print_player_stats(player_stats, role_labels)
+    print_non_role_comps(comp_stats, 1) # TODO REPLACE WITH DECLARED NUMBER FOR EASY CHANGE, would be cool to let user do it with the input file, might be complicated so ill make it optional, 1 and 3 are default
